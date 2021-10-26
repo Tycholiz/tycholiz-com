35ea4a4f0f85c62c30294f5276d0aa4a5c413725
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2021-10-26 10:09:11 -0700

    be explicit with sanity client API version

--- @app/webapp/sanity-client.ts
+++ @app/webapp/sanity-client.ts
@@ -3,5 +3,6 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: '3tpd6ok4',
   dataset: 'production',
-  useCdn: true
+  useCdn: true,
+  apiVersion: '2021-10-26',
 })
