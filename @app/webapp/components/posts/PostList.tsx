476fad0dac6e526243f01e39b3ef197dcbcf8a0c
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2021-11-04 15:17:59 -0700

    style PostSnippet list

--- @app/webapp/components/posts/PostList.tsx
+++ @app/webapp/components/posts/PostList.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import styled from 'styled-components'
-import { List, Paragraph } from '../common'
+import { List, Paragraph, Anchor } from '../common'
 
 
 type Props = {
@@ -13,29 +13,61 @@ type Props = {
   }[]
 }
 
-const InnerWrapper = styled.div``
+const Wrapper = styled.ul`
+  list-style-type: none;
+  padding: 0;
+`
+
+const InnerWrapper = styled.div`
+  padding-top: 1em;
+  padding-bottom: 1em;
+  cursor: pointer;
+
+  &:hover {
+    background: ${({ theme }) => theme.color.grayscale[6]};
+  }
+`
+
+const Title = styled.span`
+`
+
+const Subtitle = styled.div`
+  color: ${({ theme }) => theme.color.grayscale[2]};
+  font-style: italic;
+  margin-top: 0.3em;
+`
+
+const DateLabel = styled.div`
+  float: right;
+  font-size: 0.8em;
+  color: ${({ theme }) => theme.color.grayscale[4]};
+
+  @media (max-width: 700px) {
+    display: none;
+  }
+`
 
 export const PostList = ({
   posts = [],
 }: Props) => {
   return (
-    <List>
+    <Wrapper>
       {posts.map(
         ({ _id, title = '', subtitle = '', slug = '', _updatedAt = '' }) =>
           slug && (
             <li key={_id}>
               <InnerWrapper>
                 <Link href="/posts/[slug]" as={`/posts/${slug.current}`}>
-                  <a>
-                    <Paragraph>{title}</Paragraph>
-                    <Paragraph>{subtitle}</Paragraph>
-                  </a>
+                  <Anchor>
+                    <DateLabel>{new Date(_updatedAt).toDateString()}</DateLabel>
+                    <Title>{title}</Title>
+                    <Subtitle>{subtitle}</Subtitle>
+                  </Anchor>
                 </Link>{' '}
-                ({new Date(_updatedAt).toDateString()})
               </InnerWrapper>
             </li>
           )
       )}
-    </List>
+    </Wrapper>
   )
 }
