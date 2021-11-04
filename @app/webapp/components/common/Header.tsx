80fb7e5f24a96bf291ff7ebba7e579f24468d70f
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2021-11-04 15:17:38 -0700

    style nav buttons

--- @app/webapp/components/common/Header.tsx
+++ @app/webapp/components/common/Header.tsx
@@ -1,19 +1,30 @@
 import Link from 'next/link'
 import styled from 'styled-components'
-import { List, Heading, HorizontalRule } from '.'
+import { List, Heading, HorizontalRule, Anchor } from '.'
 
 
 const InnerWrapper = styled.nav`
   padding: 0.4em 2em;
 `
 
+const Bullet = styled.span`
+  padding-left: 0.5em;
+  padding-right: 0.5em;
+  color: ${({ theme }) => theme.color.primary[1]};
+`
+
 const NavList = styled(List)`
   display: flex;
   padding-left: 0;
 
-  & li {
-    padding-right: 1em;
+  & a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.color.primary[0]};
   }
+`
+
+const NavListItem = styled.a`
+  color: ${({ theme }) => theme.color.primary[0]};
 `
 
 export const Header = () => {
@@ -26,15 +37,21 @@ export const Header = () => {
         <NavList>
           <li>
             <Link href="/posts" as={`/posts`}>
-              <a>THOUGHTS</a>
+              <a>thoughts</a>
             </Link>
           </li>
+          <Bullet>&bull;</Bullet>
           <li>
             <Link href="https://tycholiz.github.io/Digital-Garden/" as={`https://tycholiz.github.io/Digital-Garden/`}>
-              <a>SECOND BRAIN</a>
+              <a>second brain</a>
             </Link>
           </li>
-          <li>NOW</li>
+          <Bullet>&bull;</Bullet>
+          <li>
+            <Link href="https://tycholiz.github.io/Digital-Garden/" as={`https://tycholiz.github.io/Digital-Garden/`}>
+              <a>now</a>
+            </Link>
+          </li>
         </NavList>
       </InnerWrapper>
   )
