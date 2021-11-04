a9f34d1059216bb6641fb1dca8e677a49a11c927
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2021-11-04 14:48:51 -0700

    adjust Paragraph letter/line spacing

--- @app/webapp/components/common/Paragraph.ts
+++ @app/webapp/components/common/Paragraph.ts
@@ -4,6 +4,7 @@ export const Paragraph = styled.p`
   font-family: ${({ theme }) => theme.font.paragraph};
   color: ${({ theme }) => theme.color.grayscale[1]};
   font-size: 1rem;
-  line-height: 1.3;
+  line-height: 1.5;
+  letter-spacing: 0.3px;
   margin: 1rem 0 0;
 `
