31ad94a86b63cc044800e4edea69cdb3c89dbdae
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2021-11-04 14:49:07 -0700

    adjust Heading font-weight

--- @app/webapp/components/common/Heading.ts
+++ @app/webapp/components/common/Heading.ts
@@ -10,7 +10,7 @@ const fontSize = ({ level }: Props) => `${0.75 + (1 * (1 / level))}rem`
 
 const styles = css`
   font-family: ${({ theme }) => theme.font.heading};
-  font-weight: 500;
+  font-weight: 700;
   font-size: ${fontSize};
   margin: 0;
   margin-top: 0.85714em;
