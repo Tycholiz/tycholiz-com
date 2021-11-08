b051d220aa20ad42810b210ebf4d48e607c87f74
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2021-11-08 10:48:59 -0800

    design match: horizontal rule

--- @app/webapp/components/common/HorizontalRule.ts
+++ @app/webapp/components/common/HorizontalRule.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const HorizontalRule = styled.hr`
-  border: 1px solid ${({ theme }) => theme.color.grayscale[3]};
-  border-width: 0 0 1px;
+  border: 1px solid ${({ theme }) => theme.color.grayscale[5]};
+  border-width: 0 0 2px;
   width: 100%;
 `
