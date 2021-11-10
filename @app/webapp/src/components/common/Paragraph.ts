5483203a5760a171091a092a44ffa5584b9d78e5
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2021-11-10 11:33:58 -0800

    adjust down paragraph default text size

--- @app/webapp/src/components/common/Paragraph.ts
+++ @app/webapp/src/components/common/Paragraph.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Paragraph = styled.p`
   color: ${({ theme }) => theme.color.grayscale[1]};
-  font-size: 1rem;
+  font-size: 0.9rem;
   line-height: 1.5;
   letter-spacing: 0.3px;
   margin: 1rem 0 0;
