da89b52b74cc1954637f60c9d3e65015acda3b76
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2021-11-10 13:02:03 -0800

    give Anchor cursor pointer

--- @app/webapp/src/components/common/Anchor.ts
+++ @app/webapp/src/components/common/Anchor.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 export const Anchor = styled.a`
   color: ${({ theme }) => theme.color.primary[0]};
+  cursor: pointer;
 
   &:visited {
     color: ${({ theme }) => theme.color.primary[1]};
