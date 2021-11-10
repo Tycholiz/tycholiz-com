28c56931b935644c222cef19f08439c09417da73
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2021-11-09 22:23:20 -0800

    remove author photo and categories from post page

--- @app/webapp/src/components/posts/Post.tsx
+++ @app/webapp/src/components/posts/Post.tsx
@@ -1,7 +1,6 @@
 import BlockContent from '@sanity/block-content-to-react'
-import imageUrlBuilder from '@sanity/image-url'
-import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 import client from '../../../sanity-client'
+import { Paragraph, Heading } from '../common'
 
 type Props = {
   title: string
@@ -11,42 +10,21 @@ type Props = {
   body: any[]
 }
 
-function urlFor (source: SanityImageSource) {
-  return imageUrlBuilder(client).image(source)
-}
-
 export const Post = ({
   title = 'Missing title',
   name = 'Missing name',
   categories,
-  authorImage,
   body = [],
 }: Props) => {
   return (
     <article>
-      <h1>{title}</h1>
-      <span>By {name}</span>
-      {categories && (
-        <ul>
-          Posted in
-          {categories.map(category => <li key={category}>{category}</li>)}
-        </ul>
-      )}
-      {authorImage && (
-        <div>
-          <img
-            src={urlFor(authorImage)
-              .width(50)
-              .url()}
-          />
-        </div>
-      )}
+      <Heading>{title}</Heading>
+      <Paragraph>By {name}</Paragraph>
       <BlockContent
         blocks={body}
         imageOptions={{ w: 320, h: 240, fit: 'max' }}
         {...client.config()}
       />
     </article>
-
   )
 }
