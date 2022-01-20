647c69d34d3e5b03698df8059b783ced2c531080
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2022-01-19 20:55:39 -0800

    hook form up to the api call. now form can create a comment that links to the post

--- @app/webapp/src/components/custom/CommentForm.tsx
+++ @app/webapp/src/components/custom/CommentForm.tsx
@@ -4,32 +4,49 @@ import { Paragraph, Heading } from '../common'
 import { PostList } from './PostList'
 import { Anchor } from '../common'
 import constants from '../../constants'
+import { useState } from 'react'
 import { SyntheticEvent } from 'react'
+import { NextApiResponse } from 'next'
 
 type Props = {
-  posts: {
-    _id: string
-    title: string
-    subtitle?: string
-    slug: any
-    _updatedAt: string
-  }[]
+	postId: string
 }
 
-export const CommentForm: React.FC<Props> = ({ posts }) => {
-	const handleSubmitComment = (e: SyntheticEvent) => {
+/* Upon 200 OK from api call to Sanity, we should replace the form with a Success message in UI */
+export const CommentForm: React.FC<Props> = ({ postId }) => {
+	const [author, setAuthor] = useState('')
+	const [email, setEmail] = useState('')
+	const [commentBody, setCommentBody] = useState('')
+
+	const handleSubmitComment = async (e: SyntheticEvent) => {
 		e.preventDefault()
+		try {
+				/* @ts-ignore */
+			await fetch('/api/createComment', {
+				method: 'POST',
+				body: JSON.stringify({
+					_id: postId,
+					author,
+					email,
+					body: commentBody,
+				}),
+				/* @ts-ignore */
+				type: 'application/json'
+			})
+		} catch(err) {
+			console.error(err)
+		}
 	}
   return (
 		<form method="post">
 			<label>your name
-				<input type="text" />
+				<input type="text" value={author} onChange={e => setAuthor(e.target.value)} />
 			</label>
 			<label>your email (private for my eyes only)
-				<input type="text" />
+				<input type="text" value={email} onChange={e => setEmail(e.target.value)} />
 			</label>
 			<label>comment
-				<input type="text" />
+				<input type="text" value={commentBody} onChange={e => setCommentBody(e.target.value)} />
 			</label>
 			<button onClick={handleSubmitComment}>Save</button>
 		</form>
