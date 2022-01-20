0ecd5c833a0b8fb7989be32e8b339bcb4af3f5f8
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2022-01-19 20:39:06 -0800

    start using env variables

--- @app/webapp/sanity-client.ts
+++ @app/webapp/sanity-client.ts
@@ -1,8 +1,9 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-  projectId: '3tpd6ok4',
-  dataset: 'production',
-  useCdn: true,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  useCdn: process.env.NODE_ENV === 'production',
+  token: process.env.SANITY_API_TOKEN,
   apiVersion: '2021-10-26',
 })
