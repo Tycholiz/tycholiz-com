3e42b587bbdf35cde2dd8fb7db45317b92923b09
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2022-01-20 17:34:59 -0800

    add small prop to Text component

--- @app/webapp/src/components/common/Text.ts
+++ @app/webapp/src/components/common/Text.ts
@@ -4,11 +4,12 @@ import { Theme } from '../../styles/theme'
 type Props = {
   theme: Theme
   bold?: boolean
+  small?: boolean
 }
 
 export const Text = styled.span<Props>`
   color: ${({ theme }) => theme.color.grayscale[3]};
   font-weight: ${({ bold }) => bold ? 'bold' : 'normal'};
-  font-size: 0.8rem;
+  font-size: ${({ small }) => small ? '0.6rem' : '0.8rem'};
   letter-spacing: 0.3px;
 `
