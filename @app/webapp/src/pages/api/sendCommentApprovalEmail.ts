6c4f5ae1c276d68c65dd4283bdef6888a833dc40
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2022-01-22 11:27:50 -0800

    add first iteration of email endpoint

---
+++ @app/webapp/src/pages/api/sendCommentApprovalEmail.ts
@@ -0,0 +1,52 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import mailjet from 'node-mailjet'
+import approveCommentEmailTemplate from '../../static/approveCommentEmailTemplate'
+
+
+// const mailjetClient = mailjet.connect(`${process.env.MAILJET_API_KEY}`, `${process.env.MAILJET_API_SECRET}`, {version: 'v3.1'})
+const mailjetClient = mailjet.connect(`${process.env.MAILJET_API_KEY}`, `${process.env.MAILJET_API_SECRET}`)
+
+type Data = {
+  name: string
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  console.log(req.body);
+
+  const request = mailjetClient
+    .post("send", { 'version': 'v3.1' })
+    .request({
+      "Messages": [
+        {
+          "From": {
+            "Email": "tycholiz22@hotmail.com",
+            "Name": "Kyle"
+          },
+          "To": [
+            {
+              "Email": "tycholizkyle@gmail.com",
+              "Name": "Kyle"
+            },
+            {
+              "Email": "tycholiz@hotmail.com",
+              "Name": "Kyle"
+            }
+          ],
+          "Subject": "Greetings from Mailjet.",
+          "TextPart": "My first Mailjet email",
+          "HTMLPart": approveCommentEmailTemplate,
+          "CustomID": "AppGettingStartedTest"
+        }
+      ]
+    })
+  request
+    .then((result) => {
+      console.log(JSON.stringify(result.body, null, 2))
+    })
+    .catch((err) => {
+      console.log(err.statusCode)
+    })
+
+  res.status(200).json({ name: 'John Doe' })
+}
