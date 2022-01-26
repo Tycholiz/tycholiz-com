7b7eb97ad81fa04048ef1d965d7e81c0c997a620
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2022-01-26 08:32:39 -0800

    fix the api endpoint to reflect actual structure

--- @app/webapp/src/static/generateApproveCommentTemplate.js
+++ @app/webapp/src/static/generateApproveCommentTemplate.js
@@ -1,7 +1,7 @@
 export default (data) => `
 	<h2>New comment from ${data.author} (${data.email}), on post </h2>
 	<p>${data.body}</p>
-	<a href="https://kyletycholiz.com/api/approveComment?id=${data._id}">Approve comment</a>
+	<a href="https://kyletycholiz.com/api/approveComment?_id=${data._id}">Approve comment</a>
 `
 
 // https://3tpd6ok4.api.sanity.io/v2021-06-07/data/mutate/production
