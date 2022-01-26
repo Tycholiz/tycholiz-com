10a5fc795f21a68331ead4e50bf7420ecf5237b7
Kyle Tycholiz <39745457+Tycholiz@users.noreply.github.com>
2022-01-26 08:30:42 -0800

    add approveComment api endpoint, since server token cannot be used on the client

---
+++ @app/webapp/src/pages/api/approveComment.ts
@@ -0,0 +1,28 @@
+import client from '../../../sanity-client'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+
+interface Response {
+  message: string
+  err?: any
+}
+
+export default async function createComment(req: NextApiRequest & { params: any }, res: NextApiResponse<Response>) {
+  const { _id } = req.query
+  
+  try {
+    await client.patch(_id as string)
+      .set({ isApproved: true })
+      .commit()
+      .then((updatedComment: any) => {
+        console.log('updatedComment', updatedComment);
+      })
+      .catch((err: Error) => {
+        console.error('Oh no, the update failed: ', err.message)
+      })
+  } catch (err) {
+    console.error(err)
+    return res.status(500).json({ message: `Couldn't approve comment`, err })
+  }
+  return res.status(200).json({ message: 'Comment submitted' })
+}
